06d6c36eaef70107ec21584fa320b9886f60eada
Pavel Suk <pavelsuk98@gmail.com>
2023-09-10 17:44:26 +0200

    project init example

---
+++ src/events/events.gateway.ts
@@ -0,0 +1,40 @@
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  WsResponse,
+} from '@nestjs/websockets';
+import { from, interval, Observable } from 'rxjs';
+import { debounceTime, map, take } from 'rxjs/operators';
+import { Server } from 'socket.io';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+export class EventsGateway {
+  @WebSocketServer()
+  server: Server;
+
+  @SubscribeMessage('events')
+  findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
+    return from([1, 2, 3]).pipe(
+      map((item) => ({ event: 'events', data: item })),
+    );
+  }
+
+  @SubscribeMessage('timersData')
+  getTimerCount(): Observable<WsResponse<number>> {
+    return interval(1000).pipe(
+      take(60),
+      map((item) => ({ event: 'timersData', data: item })),
+    );
+  }
+
+  @SubscribeMessage('identity')
+  async identity(@MessageBody() data: number): Promise<number> {
+    return data;
+  }
+}
